fd28f993ee17d2bddf506a5e1d240dc36962756c
Alexander Morgan <alexanderpmorgan@gmail.com>
2022-08-12 11:42:36 -0400

    Add commented-out post request example

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,8 @@
 const router = require('express').Router()
 const Student = require('../models/student')
+// const bcrypt = require('bcryptjs');
+// const jwt = require('jsonwebtoken');
+
 
 
 router.get('/login/:email/:password', async (req, res) => {
@@ -14,10 +17,38 @@ router.get('/login/:email/:password', async (req, res) => {
     })
 });
 
-// Example code that would write this hard-coded user to the database:
-// const user_ = new Student({name: 'Jacob Rader', email: 'jacobhrader@gmail.com', password: 'osmosis'})
-// user_.save((err, doc) => {
-//   console.log({err, doc});
-// })
+// router.post('/register', async (req, res) => {
+//     // TODO: do schema validation here
+//     // Validate fields before registering a new user
+//     // Check if the email is already in the database.
+//     const existingUser = await User.findOne({email: req.body.email});
+//     if (existingUser) {
+//         if (Boolean(existingUser.isConfirmed)) {  // already registered and confirmed
+//             return res.json({message: 'This email has already been registered.'})
+//         } else {  // already registered but not confirmed
+//             return res.json({message: "This email was already registered but never confirmed."})
+//         }
+//     }
+
+//     // Hash password
+//     const salt = await bcrypt.genSalt(10);
+//     const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+//     //Create a new user
+//     const userInfo = {
+//         email: req.body.email,
+//         password: hashedPassword,
+//         isConfirmed: false,
+//         meta: req.body.meta,
+//     };
+//     User.create(userInfo)
+//     .then(savedUser => {
+//         // Send the new user an email to confirm their info.
+//         sendEmail(savedUser.email, templates.confirm(savedUser._id))
+//     }).then(() => res.json({ msg: msgs.confirm })
+//     ).catch(err => console.log('User.create error:\n', err))
+//     });
+
+
 
 module.exports = router;
