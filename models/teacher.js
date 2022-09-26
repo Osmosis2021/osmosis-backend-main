496549325d12b106b8b23c7147c8c782066c11be
Jacob Rader <jacob.rader@baruchmail.cuny.edu>
2022-09-26 11:05:58 -0400

    teacher schema

--- models/teacher.js
+++ models/teacher.js
@@ -16,6 +16,59 @@ const TeacherSchema = new mongoose.Schema(
 			type: String,
 			required: true,
 		},
+		industry: {
+			type: String,
+			required: true,
+		},
+		generalTag: {
+			type: String,
+			required: true
+		 },
+		specificTags: {
+			type: String,
+		},
+		firstName: {
+			type: String,
+			required: true
+		 },
+		lastName: {
+			type: String,
+			required: true
+		 },
+		length: {
+			type: String,
+			required: true
+		 },
+		time: {
+			type: String,
+			required: true
+		 },
+		
+		latitude: {
+			type: Number,
+			required: true
+		 },
+		longitude: {
+			type: Number,
+			required: true
+		 },
+		
+		capacity: {
+			type: Number,
+			required: true
+		 },
+		price: {
+			type: Number,
+			required: true
+		 },
+		stars: {
+			type: Number,
+			required: true
+		 },
+		 description: {
+			type: String
+		 }
+
 	},
 	{
 		timestamps: true,
@@ -32,3 +85,10 @@ const TeacherSchema = new mongoose.Schema(
 // export Teacher Schema
 const Teacher = mongoose.model('Teacher', TeacherSchema);
 module.exports = Teacher;
+
+
+
+	
+// },
+// "description": "I love tennis. It's a lot of fun."
+// }
