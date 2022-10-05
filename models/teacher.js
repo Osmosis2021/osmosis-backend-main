0bd46cf6a10f5a7d0a976854314089b456e33a5f
Alexander Morgan <alexanderpmorgan@gmail.com>
2022-10-05 12:10:13 -0400

    Add userName to teacher schema

--- models/teacher.js
+++ models/teacher.js
@@ -11,6 +11,10 @@ const TeacherSchema = new mongoose.Schema(
 			type: String,
 			required: true
 		 },
+		 userName: {
+			type: String,
+			required: true
+		 },
 		email: {
 			type: String,
 			required: true,
