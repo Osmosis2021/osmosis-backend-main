a3e525218ea03ff0765a32c9ffdb0631b2cbbd1e
Jacob Rader <jacob.rader@baruchmail.cuny.edu>
2022-10-20 10:36:23 -0400

    Added route for to get course data from database to populate profile

--- models/course.js
+++ models/course.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const Review = require('./review').schema;
 // const mongoose = require('../db/connection');
 // const Teacher = require('./teacher').schema;
 
@@ -8,6 +7,7 @@ const CourseSchema = new mongoose.Schema({
     courseTitle: String,
 	courseDescription: String,
 	subject: String,
+    userName: String,
     capacity: Number,
     price: Number,
     tag: [String],
@@ -17,8 +17,7 @@ const CourseSchema = new mongoose.Schema({
 	// images: [],
     timeOfDay: String,
 	online: Boolean,
-	reviews: [Review],
-	rating: Number,
+	
 	// students: [{type: mongoose.Schema.Types.ObjectId, ref: 'Student'}],
     owner: {
         type: mongoose.Schema.Types.ObjectId,
@@ -28,5 +27,4 @@ const CourseSchema = new mongoose.Schema({
 });
 
 const Course = mongoose.model('Course', CourseSchema);
-
 module.exports = Course;
