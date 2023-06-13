fb0ec5a6a6b1bcba62b8da998ba5c286456b7e82
Jacob Rader <jacob.rader@baruchmail.cuny.edu>
2023-06-13 16:06:51 -0400

    Added live stripe api keys and dotenv files

--- middleware/stripe.js
+++ middleware/stripe.js
@@ -4,17 +4,16 @@ const Course = require('../models/course');
 const Booking = require('../models/booking');
 const express = require('express');
 const bodyParser = require('body-parser');
-const STRIPE_PUBLISHABLE_KEY = 'pk_test_51NEDr1EXMtM9g584HjgedDTZJQK6VCS810wg1mhHV4PigyXkFv2js5E8FSF7gEvqERXpAihzdEQdars4D7GyxlI600TaSVvn2v';
+const dotenv = require('dotenv')
+dotenv.config()
 const STRIPE_WEBHOOK_SECRET = 'whsec_78e5b3362715eaa336fb1812d63371407efab34c48565079771b0ad141ad74a0'
 const CLIENT_URL = 'http://localhost:3000';
 
-// const stripe = Stripe(process.env.STRIPE_API_TEST_KEY);
-    const stripe = Stripe('sk_test_51NEDr1EXMtM9g5843QsnEpiIAtZU9jFQ8kXabGCCDuapFuYR79weeKf14YFSY7PlLBtDcSFRm2Oz5D21zJQKogKe00I53AamYY');
-    
+const stripe = Stripe(process.env.STRIPE_API_TEST_KEY)
 
     router.get('/config', (req, res) => {
         res.send({
-        publishableKey: STRIPE_PUBLISHABLE_KEY,
+        publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
         });
     });
 
