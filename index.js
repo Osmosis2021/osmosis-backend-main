ae67dc32adcd3e703bb5dd0f5b3929ef920330d8
Alexander Morgan <alexanderpmorgan@gmail.com>
2023-06-27 10:53:40 -0400

    Forward http requests to https
    Related #61

--- index.js
+++ index.js
@@ -15,7 +15,12 @@ dotenv.config()
 const app = express();
 const allowList = ['https://getosmosis.io', 'https://osmosis.herokuapp.com', '/']
 if(process.env.NODE_ENV !== 'production') {
-    allowList.push('http://localhost:3000')
+    app.use((req, res, next) => {
+        if (req.header('x-forwarded-proto') !== 'https')
+            res.redirect(`https://${req.header('host')}${req.url}`)
+        else
+            next()
+    })
 }
 
 app.use(cors({ origin: allowList, credentials: true }))
