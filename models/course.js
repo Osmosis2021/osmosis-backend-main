65dfb2ebc4a704f436ed9674c567b727b6657716
Jacob Rader <jacob.rader@baruchmail.cuny.edu>
2023-07-25 12:14:36 -0400

    Changed feedback to an array of objects with rating, review, and studentID

--- models/course.js
+++ models/course.js
@@ -21,11 +21,11 @@ const CourseSchema = new mongoose.Schema({
     capacity: Number,
     pricePerStudent: Number,
     tags: [String],
-    feedback: {
-        rating: [Number],
-        reviews: [String],
+    feedback: [{
+        rating: Number,
+        reviews: String,
         studentID: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    },
+    }],
     // city: String,
     // zipCode: Number,
     // address: String,
