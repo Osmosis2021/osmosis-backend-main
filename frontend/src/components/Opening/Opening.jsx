7d3c7d3ef5cfec2068be8cd1ad04eab0e31e812f
Alexander Morgan <alexanderpmorgan@gmail.com>
2023-09-27 12:32:47 -0400

    Auto focus username input

--- frontend/src/components/Opening/Opening.jsx
+++ frontend/src/components/Opening/Opening.jsx
@@ -77,8 +77,10 @@ const Opening = () => {
 			
 						<Grid item>
 							{
-								isWrong ? <TextField variant='outlined' label="Email or Username" placeholder='Email or Username' error fullWidth onChange={handleChangeEmail} style={{marginBottom:'12px'}}/> :
-								<TextField variant='outlined' label="Email or Username" placeholder='Email or Username' fullWidth onChange={handleChangeEmail} />
+								isWrong ? <TextField variant='outlined' label="Email or Username" placeholder='Email or Username'
+											error fullWidth onChange={handleChangeEmail} style={{marginBottom:'12px'}}/> :
+								<TextField autoComplete='off' variant='outlined' label="Email or Username" placeholder='Email or Username'
+									fullWidth onChange={handleChangeEmail} inputRef={input => input && input.focus()}/>
 							}
 							{
 								isWrong ? <TextField variant='outlined' type="password" label="Password" placeholder='Password' error fullWidth onChange={handleChangePassword} helperText="Incorrect entry."/> :
