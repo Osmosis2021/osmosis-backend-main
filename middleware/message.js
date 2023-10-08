73f106e0f6bfb4e5e014e26280187ec063d0c605
Alexander Morgan <alexanderpmorgan@gmail.com>
2023-10-08 13:55:30 -0400

    Remove misspelled import

--- middleware/message.js
+++ middleware/message.js
@@ -1,82 +1,63 @@
 const router = require('express').Router()
 const asyncHandler = require("express-async-handler");
 const User = require('../models/user');
-const Messgae = require('../models/message');
 const Chat = require('../models/chat');
 const jwt = require('jsonwebtoken');
 const Message = require('../models/message');
-const jwtSecret = 'randomString';
+const dotenv = require('dotenv')
+dotenv.config()
+const jwtSecret = process.env.ACCESS_TOKEN_SECRET
 
 
 // SENDING MESSAGE
 router.post('/sendMessage', asyncHandler(async (req, res) => {
-
     const { chatId, content } = req.body
-    const {token} = req.cookies;
-
-    console.log('chatId', chatId)
-    console.log('content', content)
-
+    const accessToken = req.cookies?.accessToken || req.cookies?.token
     if (!content || !chatId) {
         console.log('invalid data passed into request')
         return res.sendStatus(400);
     }
-
-    // Verify the JWT token to get user data
-    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    // Verify the JWT accessToken to get user data
+    jwt.verify(accessToken, jwtSecret, {}, async (err, userData) => {
         if (err) {
-            // Handle token verification error (e.g., invalid token)
+            // Handle accessToken verification error (e.g., invalid accessToken)
             return res.status(401).json({ error: 'Unauthorized' });
         }
-
         var newMessage = {
             sender: userData.id,
             content: content,
             chat: chatId
         }
-
         try {
             var message = await Message.create(newMessage);
-
             message = await message.populate('sender', 'userName firstName lastName email profileImage');
             message = await message.populate('chat');
             message = await User.populate(message, {
                 path: "chat.users",
                 select:"userName firstName lastName email profileImage"
             });
-
             await Chat.findByIdAndUpdate(req.body.chatId, {
                 latestMessage: message,
             });
             res.json(message)
-            
         } catch (err) {
             res.status(400)
             throw new Error (err.message)
         }
-
-
-
-
-
     })
 }))
 
 
 // FETCHING MESSAGES
-
 router.get('/allMessages/:chatId', asyncHandler(async (req, res) => {
     const {chatId} = req.params;
-    const {token} = req.cookies;
-    console.log('in backend route allmessages/:chatId with this id:', chatId)
-
-    // Verify the JWT token to get user data
-    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    const accessToken = req.cookies?.accessToken || req.cookies?.token
+    // Verify the JWT accessToken to get user data
+    jwt.verify(accessToken, jwtSecret, {}, async (err, userData) => {
         if (err) {
-            // Handle token verification error (e.g., invalid token)
+            // Handle accessToken verification error (e.g., invalid accessToken)
             return res.status(401).json({ error: 'Unauthorized' });
         }
-
         try {
             const messages = await Message.find({ chat: chatId }).populate(
                 'sender', 'userName profileImage email')
@@ -86,16 +67,8 @@ router.get('/allMessages/:chatId', asyncHandler(async (req, res) => {
             res.status(400)
             throw new Error (error.message)
         }
-
-
-
-
-
-
-
     })
 }))
-
 
 
 module.exports = router;
