16619453ee94ad6690c30c7b3f83b4d5f478209d
Alexander Morgan <alexanderpmorgan@gmail.com>
2023-10-15 10:48:38 -0400

    Remove unused imports

--- frontend/src/components/Dialog.jsx
+++ frontend/src/components/Dialog.jsx
@@ -8,8 +8,6 @@ import ListItemAvatar from '@mui/material/ListItemAvatar';
 import ListItemText from '@mui/material/ListItemText';
 import DialogTitle from '@mui/material/DialogTitle';
 import Dialog from '@mui/material/Dialog';
-import PersonIcon from '@mui/icons-material/Person';
-import AddIcon from '@mui/icons-material/Add';
 import Typography from '@mui/material/Typography';
 import { blue } from '@mui/material/colors';
 
