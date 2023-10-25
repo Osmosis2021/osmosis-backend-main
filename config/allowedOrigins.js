154c111a3828fbd1ed88f5509f73d2a99f5871ee
Alexander Morgan <alexanderpmorgan@gmail.com>
2023-10-25 13:08:07 -0400

    Update allowedOrigins list when not in prod

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,12 +2,13 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 const allowedOrigins = [
-    'https://getosmosis.io',
-    'http://localhost:8126'
+    'https://getosmosis.io'
 ];
 
 if(process.env.NODE_ENV !== 'production') {
     allowedOrigins.push('http://localhost:3000')
+    allowedOrigins.push('http://localhost:8100')
+    allowedOrigins.push('capacitor://localhost')
 }
 
 module.exports = allowedOrigins;
