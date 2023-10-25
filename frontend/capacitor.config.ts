e50b1b83090cc3ae6ef91f35838c31ef3a066793
Alexander Morgan <alexanderpmorgan@gmail.com>
2023-10-25 13:26:53 -0400

    Update mobile app deps and files

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -1,10 +1,12 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'webpage',
+  appId: 'io.getosmosis',
+  appName: 'osmosis',
   webDir: 'build',
-  bundledWebRuntime: false
+  server: {
+    androidScheme: 'https'
+  }
 };
 
 export default config;
