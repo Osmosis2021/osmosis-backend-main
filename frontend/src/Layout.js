3b15d30f0d69ebaa179e258420f3709ef4a0e1ff
Alexander Morgan <alexanderpmorgan@gmail.com>
2023-11-08 12:41:12 -0500

    Try to fix android browsers with position: fixed

--- frontend/src/Layout.js
+++ frontend/src/Layout.js
@@ -5,7 +5,6 @@ import './Layout.css'
 
 const Layout = () => {
     const {platform} = useStore()
-    const _style = platform === 'web' ?  {} : {padding: '40px 0 0'}
     return(
         <main className={`App Layout-${platform}`}>
             <Outlet />
