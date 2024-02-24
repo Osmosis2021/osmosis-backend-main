9a4374434670a8d8630e250682622c4e98cec7b2
Jacob Rader <jacob.rader@baruchmail.cuny.edu>
2024-02-24 13:32:53 -0500

    Linked cards to their respective course pages

--- frontend/src/components/Explore/Explore.jsx
+++ frontend/src/components/Explore/Explore.jsx
@@ -3,11 +3,13 @@ import React, {useState, useEffect} from 'react';
 import SessionCard from '../SessionCard/SessionCard';
 import axios from 'axios';
 import useStore from "../../store"
-
+import { useNavigate } from 'react-router-dom';
 
 export default function Explore() {
     const {backendURL} = useStore()
     const [classes, setClasses] = useState([]);
+    const navigate = useNavigate()
+
     useEffect(()=>{
         axios.get(`${backendURL}course/getClasses`).then(response => {
             setClasses(response.data)
@@ -26,19 +28,21 @@ export default function Explore() {
                 </Stack>
                 <Grid container direction='row' spacing={2}>
                     {classes?.length > 0 && classes?.map(course => (
-                        <Grid item xs={12} sm={6} md={4} lg={3}>
-                            <SessionCard 
-                                images={course?.images[0]?.url} 
-                                industry={course?.industry}
-                                courseTitle={course?.courseTitle}
-                                firstName={course?.teacherID?.firstName}
-                                lastName={course?.teacherID?.lastName}
-                                tags={course?.tags}
-                                price={course?.pricePerStudent}
-                                icon={course?.industry}
-                                profileImage={course?.teacherID?.profileImage?.url}
-                                capacity={course?.capacity}
-                            />
+                        <Grid item xs={12} sm={6} md={4} lg={3} key={course?._id}>
+                            <div onClick={() => navigate(`/teachers/${course?.teacherID?.userName}/${course?._id}`)}>
+                                <SessionCard 
+                                    images={course?.images[0]?.url} 
+                                    industry={course?.industry}
+                                    courseTitle={course?.courseTitle}
+                                    firstName={course?.teacherID?.firstName}
+                                    lastName={course?.teacherID?.lastName}
+                                    tags={course?.tags}
+                                    price={course?.pricePerStudent}
+                                    icon={course?.industry}
+                                    profileImage={course?.teacherID?.profileImage?.url}
+                                    capacity={course?.capacity}
+                                />
+                            </div>
                         </Grid>
                     ))}
                 </Grid>
